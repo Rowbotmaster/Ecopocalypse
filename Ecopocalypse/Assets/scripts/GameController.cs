8ea26a588d4a41c23d89112df91c0b86f6fa58df
Rowbotmaster <39174581+Rowbotmaster@users.noreply.github.com>
2019-04-26 15:36:08 +1000

    first commit

---
+++ Ecopocalypse/Assets/scripts/GameController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public static GameController Instance;
+
+    public int electricity = 100;
+    public int food = 100;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
