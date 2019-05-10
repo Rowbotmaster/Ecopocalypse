010b92ba6fd646fae3c3e88adff4f98a10a14d0a
Rowbotmaster <39174581+Rowbotmaster@users.noreply.github.com>
2019-05-10 14:21:51 +1000

    car choice

---
+++ Ecopocalypse/Assets/scripts/CarChoice.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CarChoice : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ElectricCarChoice()
+    {
+        GameController.Instance.petrolCar = false;
+        SceneManager.LoadScene(1);
+    }
+
+    public void PetrolCarChoice()
+    {
+        GameController.Instance.petrolCar = true;
+        SceneManager.LoadScene(1);
+    }
+}
